0b2fc07237b3dc24c40c6ccae8739639cb95cd20
Adesanmi Dayo PraiseGod <dayopraisegod@gmail.com>
2023-09-11 22:26:14 +0100

    fix static files bugs

--- hngx_two/settings/production.py
+++ hngx_two/settings/production.py
@@ -15,7 +15,7 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-# CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", cast=Csv())
+CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", cast=Csv())
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
